acc6fc70fcee4cd5b60b34ce843e02697a9a5586
djamila4 <houleyeaissata@gmail.com>
2018-03-14 20:09:18 +0000

    Ajout d'un projet

---
+++ Employe.java
@@ -0,0 +1,17 @@
+public class Employe{
+	
+	//Propriete
+	
+	private String nom;
+	private String prenom;
+	
+	//Constructeur
+	
+	public Employe(){}
+	
+	public Employe(String nom, String prenom){
+		this.nom = nom;
+		this.prenom = prenom;
+		
+	}
+}
---
+++ Etudiant.java
@@ -0,0 +1,17 @@
+public class Etudiant{
+	
+	//Propriete
+	
+	private String nomEtudiant;
+	private String prenomEtudiant;
+	
+	//constructeur
+	
+	public Etudiant(){}
+	
+	public Etudiant(String nomEtudiant, String prenomEtudiant){
+		this.nomEtudiant = nomEtudiant;
+		this.prenomEtudiant = prenomEtudiant;
+		
+	}
+}
